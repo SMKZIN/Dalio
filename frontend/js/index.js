932fe5d6e9cb1a4c4c0d498159b236cdcda1108e
samuel <samueldavi6306@gmail.com>
2025-10-02 22:44:29 -0300

    Comentarios escondidos

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -11,3 +11,13 @@ enjoy.addEventListener("click", () => {
         enjoy.src = "/frontend/img/heart.png"
     }
 });
+
+document.querySelectorAll(".ver-comentarios").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const container = btn.closest(".comentarios--cliente");
+    container.classList.add("active");
+    btn.style.display = "none"; // esconde o "Ver todos os comentários"
+    container.querySelector(".comentario-preview").style.display = "none"; // esconde o preview apagado
+  });
+});
+
